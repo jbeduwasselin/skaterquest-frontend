caa68eb9e0d9c97c7bf3c72d20ac24ff7754a92d
Your name <Your email>
2025-04-14 12:30:27 +0200

    login WIP

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,19 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+// import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../reducers/user";
 
 export default function LoginScreen({ navigation }) {
- return (
-   <View>
-     <Text>Login Screen</Text>
-     <Button
-       title="Go to Home"
-       onPress={() => navigation.navigate('TabNavigator')}
-     />
-   </View>
- );
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+
+  return (
+    <View>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate("TabNavigator")}
+      />
+    </View>
+  );
 }
