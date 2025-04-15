7c36ef5353c4fc4b710dab94847db071e3effd7d
Your name <Your email>
2025-04-15 16:03:26 +0200

    Restauration du component background

---
+++ components/background.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+export default function BackgroundWrapper({ children }) {
+  return (
+    <ImageBackground
+      source={require('../assets/grip skate background temporaire.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      {children}
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
