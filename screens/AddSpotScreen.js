02e1b825dc28bda26f5c0cb984defb295fcad7b7
Your name <Your email>
2025-04-17 18:00:50 +0200

    WIP ajout de spot en BDD

--- screens/AddSpotScreen.js
+++ screens/AddSpotScreen.js
@@ -14,6 +14,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateSpot } from "../reducers/spot";
 import { CameraView, Camera } from "expo-camera";
 import { useIsFocused } from "@react-navigation/native";
+import { createSpot } from "../lib/request";
 
 export default function AddSpotScreen({ navigation }) {
   const dispatch = useDispatch();
@@ -24,7 +25,8 @@ export default function AddSpotScreen({ navigation }) {
 
   // Récupération des infos depuis le store
   const user = useSelector((state) => state.user.value);
-  const spot = useSelector((state) => state.spot.value);
+  const spotLat = useSelector((state) => state.spot.value.latitude);
+  const spotLon = useSelector((state) => state.spot.value.longitude);
 
   const [spotName, setSpotName] = useState(""); // État pour enregistrer le nom donné au spot par l'utilisateur
   const [spotCategory, setSpotCategory] = useState(""); // État pour enregistrer la catégorie du spot choisie par l'utilisateur
@@ -45,10 +47,11 @@ export default function AddSpotScreen({ navigation }) {
 
   // Fonction pour enregistrer le spot
   const saveSpot = () => {
-    // Lancer la route qui enregistre le spot, en vérifiant que spotCategory n'est pas vide et que la photo a été prise.
-    // Envoyer les infos de localisation enregistrées dans le store depuis MapScreen, ainsi que les infos de spotCategory et la photo
+    // Ajouter une vérif que spotCategory n'est pas vide et qu'au moins 1 photo a été prise
 
-    //createSpot(user.token, { spotName, spot.longitude, spot.latitude, spotCategory });
+    createSpot(user.token, { spotName, spotLat, spotLon, spotCategory });
+
+    // Ajouter en BDD les photos prises (pas dans la même route que createSpot)
 
     navigation.navigate("SpotScreen");
   };
@@ -105,7 +108,7 @@ export default function AddSpotScreen({ navigation }) {
         </View>
 
         <View style={styles.cameraContainer}>
-          <Text style={styles.title}>Prends une photo du spot !</Text>
+          <Text style={styles.title}>Prends des photos du spot !</Text>
 
           {!hasPermission || !isFocused ? (
             <View>
